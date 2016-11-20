38db725c7b3a0a8a77ac9d2a1afdc4e0647aa637
Pedro Henrique de Almeida Konzen <phkonzen@gmail.com>
2016-11-20 20:30:04 -0200

    *ponto fixo;*eliminacao gaussiana;*gauss-seidel;*jacobi

---
+++ cap_linsis/codes/octave/metodo_de_jacobi/jacobi.m
@@ -0,0 +1,20 @@
+function [x]=jacobi(A,b,x,tol,N)
+  n = size(A,1);
+  x0 = x;
+  iter = 1;
+  while (iter <= N)
+    #iteracao Jacobi
+    for i = 1:n
+      x(i) = (b(i) - A(i,[1:i-1,i+1:n])*x0([1:i-1,i+1:n]))/A(i,i);
+    endfor
+    #condicao de parada
+    if (norm(x-x0,'inf')<tol)
+      return
+    endif
+    #prepara nova iteracao
+    x0 = x;
+    i += 1;
+  endwhile
+  error('num. max. iter. excedido.')  
+endfunction
+
