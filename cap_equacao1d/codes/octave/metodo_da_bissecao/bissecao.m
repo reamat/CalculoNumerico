01fa82421fe10f77c988491118e14e0d4714f1d4
phkonzen <phkonzen@gmail.com>
2017-04-19 16:57:31 -0300

    pequena correcao

--- cap_equacao1d/codes/octave/metodo_da_bissecao/bissecao.m
+++ cap_equacao1d/codes/octave/metodo_da_bissecao/bissecao.m
@@ -1,22 +1,22 @@
 function [p] = bissecao(f, a, b, TOL, N)
-  i = 1
-  fa = f(a)
+  i = 1;
+  fa = f(a);
   while (i <= N)
     #iteracao da bissecao
-    p = a + (b-a)/2
-    fp = f(p)
+    p = a + (b-a)/2;
+    fp = f(p);
     #condicao de parada
-    if ((fp == 0) | ((b-a)/2 < TOL))
-      return
+    if ((fp == 0) || ((b-a)/2 < TOL))
+      return;
     endif
     #bissecta o intervalo
-    i = i+1
+    i = i+1;
     if (fa * fp > 0)
-      a = p
-      fa = fp
+      a = p;
+      fa = fp;
     else
-      b = p
+      b = p;
     endif
   endwhile
-  error ('Num. max. de iter. excedido!')
+  error ('Num. max. de iter. excedido!');
 endfunction
