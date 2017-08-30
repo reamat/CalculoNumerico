7b16016862c5380c4ab65fd9718032e82bdf7abb
Fabio S. de Azevedo <fazedo@gmail.com>
2017-08-30 13:20:46 -0300

    correções pequenas

--- a.py
+++ a.py
@@ -3,62 +3,53 @@ from __future__ import print_function
 
 import numpy as np
 
+def euler(h,Tmax,u1):
+	itmax=np.int(Tmax/h)
+	u=np.empty(itmax+1)
+	u[0]=u1
 
-
-def euler_RK4(h,Tmax,u1):
-	u=u1
-  	itmax = Tmax/h;
 	for i in np.arange(0,itmax):
 		t=i*h
-		k1 = f(t,u)
-		k2 = f(t+h/2,u+h*k1/2)
-		k3 = f(t+h/2,u+h*k2/2)
-		k4 = f(t+h,u+h*k3)
-
-
-		u=u+h*(k1+2*k2+2*k3+k4)/6
+		k1 = f(t,u[i])
+		u[i+1] = u[i] + h*k1
 	return u
 
 
 
 
 def euler_mod(h,Tmax,u1):
-  	itmax = Tmax/h;
-	x=np.empty(itmax+1)
-	y=np.empty(itmax+1)
-	x[0]=u1[0]
-	y[0]=u1[1]
+  	itmax = np.int(Tmax/h)
+	u=np.empty(itmax+1)
+	u[0]=u1
 
 	for i in np.arange(0,itmax):
 		t=i*h
-		kx1 = (y[i])
-		ky1 = -(np.sin(x[i])+y[i])
+		k1 = f(t, u[i])
+		k2 = f(t+h, u[i]+h*k1)
+		
+		u[i+1]=u[i]+h*(k1+k2)/2
 
-		x_til = x[i] + h*kx1
-		y_til = y[i] + h*ky1
-
-		kx2 = (y_til)
-		ky2 = -(np.sin(x_til)+y_til)
-
-		x[i+1]=x[i]+h*(kx1+kx2)/2
-		y[i+1]=y[i]+h*(ky1+ky2)/2
-
-	return [x,y]
-
-
-def euler(h,Tmax,u1):
-	u=u1
-  	itmax = Tmax/h;
-	for i in np.arange(0,itmax):
-		t=i*h
-		k1 = f(t,u)
-		u = u + h*k1
 	return u
 
 
 
-def f(t,u):
-	return t-u**2
+
+def RK2_generico(h,Tmax,u1,alpha):
+  	itmax = Tmax/h;
+	u=np.empty(itmax+1)
+	u[0]=u1
+	b2=1/(2*alpha)
+	b1=1-b2
+	print(b1)
+	for i in np.arange(0,itmax):
+		t=i*h
+
+		k1 = f(t,     u[i])
+		k2 = f(t+h*alpha, u[i] + h*k1*alpha)
+
+		u[i+1] = u[i] + h*(k1*b1+k2*b2)
+	return u
+
 
 
 def RK3_classico(h,Tmax,u1):
@@ -77,12 +68,81 @@ def RK3_classico(h,Tmax,u1):
 	return u
 
 
+
+def RK4_classico(h,Tmax,u1):
+  	itmax = Tmax/h;
+	u=np.empty(itmax+1)
+	u[0]=u1
+
+	for i in np.arange(0,itmax):
+		t=i*h
+		k1 = f(t,u[i])
+		k2 = f(t+h/2,u[i]+h*k1/2)
+		k3 = f(t+h/2,u[i]+h*k2/2)
+		k4 = f(t+h,u[i]+h*k3)
+
+
+		u[i+1]=u[i]+h*(k1+2*k2+2*k3+k4)/6
+	return u
+
+
+
+
+
+
+
+
+
+def f(t,u):
+	return -0.5*u +2 +t
+	#return u+t
+	#return 2e-1* u**2
+	#return np.sqrt(u)
+
+def f_e(t,u1):
+	return 2*t +8*np.exp(-.5*t)
+	#return u1/(1-2e-1*u1*t)
+	#return (t+2)**2/4
+Tmax=1.0			#tempo maximo de simulacao
+u1=8			#condicoes iniciais na forma vetorial
+
+#sol_exata = 2+8*np.exp(-.5)
+sol_exata = f_e(Tmax,u1)#2*np.exp(Tmax)-Tmax-1
+tab=np.empty([4,4])
+err=np.empty([4,4])
+m=0
+p=0
+nomes=['Euler', 'Euler mod.', 'RK$_3$', 'RK$_4$']
+for metodo in [euler, euler_mod, RK3_classico, RK4_classico]:
+	for h in [1, 1e-1, 1e-2, 1e-3]:
+		itmax=np.int(Tmax/h)
+		sol=metodo(h,Tmax,u1)
+		erro_relativo = np.fabs((sol[itmax]-sol_exata)/sol_exata)
+		tab[m,p]=sol[itmax]
+		err[m,p]=erro_relativo
+		#print(" & %1.7f" % ( sol[itmax]),end='' )
+		#print("%1.2e & " % ( erro_relativo),end='' )
+		p=p+1
+	m=m+1
+	p=0
+	#print('')
+
+for m in [0,1,2,3]:
+	print(nomes[m],end='')
+	for p in [0,1,2,3]:
+		print(" & %1.7f" % ( tab[m,p]),end='' )
+		#print("%1.2e & " % ( erro_relativo),end='' )
+	print('\\\\')
+	print('\hline')
+
+	print('$\\varepsilon_{rel}$',end='')
+	for p in [0,1,2,3]:
+		print(" & %1.1e" % ( err[m,p]),end='' )
+		#print("%1.2e & " % ( erro_relativo),end='' )
+	print('\\\\')
+	print('\hline')
+
+
+
+#print("h=%1.0e - u(1) =~ %1.7f - erro_relativo = %1.1e" % (h, sol[itmax], erro_relativo) )
 	
-Tmax=2			#tempo maximo de simulacao
-u1=0			#condicoes iniciais na forma vetorial
-h=1e-2			#passo
-
-sol=RK3_classico(h,Tmax,u1);
-itmax=Tmax/h
-print(sol[itmax])
-
