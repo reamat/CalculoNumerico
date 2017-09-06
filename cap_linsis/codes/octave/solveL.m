5e59c1530437114937778b3e893b478f7e01b615
Rafael Sachetto <rsachetto@gmail.com>
2017-09-06 16:30:46 -0300

    Pequenas correcoes no capitulo de sistemas lineares e adicao de codigo octave

---
+++ cap_linsis/codes/octave/solveL.m
@@ -0,0 +1,7 @@
+function [x]=solveL(L,b)   #L: matriz triangular inferior
+    n=size(L,1)               #b: vetor
+    x(1)=b(1)/L(1,1)
+    for i=2:n
+        x(i)=(b(i)-L(i,1:i-1)*x(1:i-1)')/L(i,i)
+    end
+endfunction
