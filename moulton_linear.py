b5b740313c4c6b6d1e56914a6578a660aa14c0f4
Fabio S. de Azevedo <fazedo@gmail.com>
2017-10-05 17:17:44 -0300

    atualizando

--- moulton_linear.py
+++ moulton_linear.py
@@ -10,20 +10,26 @@ def Adams_Moulton_4(A,h,Tmax,u1):
 	B=np.linalg.inv(np.eye(dim)-3/8*h*A);
 
 	itmax=np.int(Tmax/h)
-	u=np.empty((dim,itmax+1))
+	u=np.empty((dim, itmax+1))
 	fn=np.empty((dim,3))
 	u[:,0]=u1
 
 	#Iniciliza com RK4
 	for i in np.arange(0,3):
 		t=i*h
-		k1 = f(t)+A.dot(u[:,i])
-		k2 = f(t+h/2) + A.dot(u[:,i]+k1*h/2)
-		k3 = f(t+h/2) + A.dot(u[:,i]+k2*h/2)
-		k4 = f(t+h)   + A.dot(u[:,i]+k3*h)
+		k1 = f(t)+np.matmul(A,u[:,i])
 
+#		print A
+#		print A.dot((u[:,1]+ (k1[0,:]+k1*h/2)).T)
+
+
+		k2 = f(t+h/2) + A.dot((u[:,i]+k1*h/2).T).T
+		k3 = f(t+h/2) + A.dot((u[:,i]+k2*h/2).T).T
+		k4 = f(t+h)   + A.dot((u[:,i]+k3*h).T).T
+
+		print k2
 		u[:,i+1]=u[:,i]+h*(k1+2*k2+2*k3+k4)/6
-		fn[:,i]=k4+ h*A.dot(u[:,i+1])
+		fn[:,i]= h*A.dot(u[:,i+1]) + f(t+h)
 		
 	for i in np.arange(0,itmax-1):
 		t=(i+1)*h
@@ -41,9 +47,11 @@ def Adams_Moulton_4(A,h,Tmax,u1):
 A=np.matrix([[1, 1],[2, 3]])
 u1=np.array([0,1])
 
+
 h=1e-1
 Tmax=1
 itmax=np.int(Tmax/h)
+
 
 Adams_Moulton_4(A,h,Tmax,u1)
 
--- resol_moulton.py
+++ resol_moulton.py
@@ -2,22 +2,39 @@ from __future__ import division
 import numpy as np
 
 
-def f(t,u):
-	return np.sqrt(1+u)
+## resolve u'(t)=l*u(t) + g
+
+def g(t):
+	return t*np.exp(-t)
 
 
 
 
-u0=0
-h=1e-1
-Tmax=1
+u0=-1
+h=1e-2
+Tmax=2
 itmax=np.int(Tmax/h)
 
 u=np.empty(itmax+1)
-u[0]=u0
+fn=np.empty(itmax+1)
 
-for i in np.arange(0,itmax):
-	x=np.sqrt(1+u[i])
+u[0]=u0
+l=-2
+#Iniciliza com Euler modificado
+k1= l*u[0] + g(0)
+k2= l*(u[0]+h*k1) + g(h)
+u[1]= u[0]+ h *(k1+k2)/2
+
+fn[0]= k1
+fn[1]= l*u[1] + g(h)
+
+
+
+for n in np.arange(0,itmax-1):
+	gn2=g((n+2)*h)
 	
-	u[i+1]= u[i]+h/8*np.sqrt(h**2 + 16 + 16*u[i] + 8*h*x)+ h/2*x + h**2/8	
-	print u[i+1]
+	u[n+2]= (u[n+1] + h/12*(8*fn[n+1]-fn[n]) + 5*h/12*gn2 ) / (1+5*h/6)
+	fn[n+2]=l*u[n+2]+gn2
+
+for n in np.arange(0,itmax+1):
+	print h*n,u[n], (h*n-1)*np.exp(-h*n)
